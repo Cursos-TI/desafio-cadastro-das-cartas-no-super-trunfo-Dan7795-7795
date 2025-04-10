e1a3cd60890e2abef48572632d775be19ea5cb37
Dan7795-7795 <danilomorais7795@gmail.com>
2025-04-10 00:26:59 +0000

    printf finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,7 +10,7 @@ char nome1[40], estado1 [30];
 float area1, pib1;
 
 // Carta 2
-int codigo2, npopulacao2, pontosturisticos2;
+int codigo2, populacao2, pontosturisticos2;
 char nome2 [40], estado2 [30];
 float area2, pib2;
 
@@ -41,9 +41,51 @@ scanf("%d", &pontosturisticos1);
 
 
 // CADASTRO DA CARTA 2
-printf("\ndigite os dados da cidade 2:");
+printf("\ndigite os dados da cidade 2:\n");
 printf("nome:");
 scanf("%s", &nome2);
+
+printf("estado:");
+scanf("%s", &estado2);
+
+printf("codigo:");
+scanf("%d", &codigo2);
+
+printf("populacao:");
+scanf("%d", &populacao2);
+
+printf("area(em km²):");
+scanf("%f", &area2);
+
+printf("PIB(em bilhoes):");
+scanf("%f", &pib2);
+
+printf("pontos turisticos:");
+scanf("%d",&pontosturisticos2);
+
+// EXIBIÇAO DAS CARTAS 
+// carta 1
+printf("\n--- carta cidade 1---\n");
+printf("nome: %s\n",nome1);
+printf("estado: %s\n", estado1);
+printf("codigo: %d\n", codigo1);
+printf("populaçao: %d\n", populacao1);
+printf("area:%.2f em km²\n", area1);
+printf("PIB: R$ %.2f bilhoes\n", pib1);
+printf("pontos turisticos:%d\n", pontosturisticos1);
+
+// carta 2
+
+
+printf("\n--- carta cidade 2---\n");
+printf("nome: %s\n",nome2);
+printf("estado: %s\n", estado2);
+printf("codigo: %d\n", codigo2);
+printf("populaçao: %d\n", populacao2);
+printf("area:%.2f em km²\n", area2);
+printf("PIB: R$ %.2f bilhoes\n", pib2);
+printf("pontos turisticos:%d\n", pontosturisticos2);
+
 
     return 0;
 }
