f9a3316757336882f502898566bc1bd5acb10c0a
Dan7795-7795 <danilomorais7795@gmail.com>
2025-04-10 21:41:34 +0000

    programafinalizado.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,7 +12,7 @@ int codigo1,populacao1, pontosturisticos1;
 char nome1[40], estado1 [30];
 float area1, pib1;
 
-// Variaveis para a segunda carta (cidade 2) 
+// Variaveis para a segunda carta (cidade 2)  
 int codigo2, populacao2, pontosturisticos2; 
 char nome2 [40], estado2 [30];
 float area2, pib2;
@@ -21,26 +21,26 @@ float area2, pib2;
 // Entrada de dados cidade 1
 // Cadastro da carta 1
 printf("digite os dados da cidade 1:\n");
-printf("nome:"); //mensagem para o usuario
-scanf("%s",&nome1); // ler o nome da cidade1
+printf("nome:"); //Solicita ao usuário o nome da cidade1
+scanf("%s",&nome1); //  Armazena o nome da cidade1 digitado pelo usuário
 
-printf("estado:");
-scanf("%s", &estado1); //ler o estado da cidade1
+printf("estado:");  // Solicita ao usuario o estado da cidade1
+scanf("%s", &estado1); // Armazena o estado da cidade1 digitado pelo usauario
 
-printf("codigo:");
-scanf("%d", &codigo1); //ler o codigo da cidade 1
+printf("codigo:");  // Solicita ao usuario o codigo da cidade1
+scanf("%d", &codigo1); // Armazena o codigo da cidade1 digitado pelo usauario
 
-printf("populacao:");
-scanf("%d", &populacao1); // ler a populaçao da cidade 1
+printf("populacao:");  // Solicita ao usuario a populaçao da cidade1
+scanf("%d", &populacao1);  // Armazena a populaçao da cidade1 digitada pelo usauario
 
-printf("area(em km²):");
-scanf("%f",&area1); // ler a area da cidade 1
+printf("area(em km²):"); // Solicita ao usuario a area da cidade1
+scanf("%f",&area1); // Armazena a aréa da cidade1 digitada pelo usuario
 
-printf("PIB (em bilhoes):"); 
-scanf("%f", &pib1); // ler o pib da cdade 1
+printf("PIB (em bilhoes):");  // Solicita ao usuario o PIB da cidade1
+scanf("%f", &pib1); // Armazena o PIB da cidade1 digitado pelo usuario
 
-printf("pontos turisticos:");
-scanf("%d", &pontosturisticos1); // ler os pontos turisticos da cidade 1
+printf("pontos turisticos:"); // Solicita ao usuario os pontos turisticos da cidade1
+scanf("%d", &pontosturisticos1); // Armazena os pontos turisticos da cidade1 digitada pelo usuario
 
 // Entrada de dados cidade 2
 // Cadastro da carta 2
@@ -70,17 +70,17 @@ scanf("%d",&pontosturisticos2);
 
 // Carta 1 -- cidade 1
 printf("\n--- carta cidade 1---\n");
-printf("nome: %s\n",nome1); // mostra o nome 
-printf("estado: %s\n", estado1);
-printf("codigo: %d\n", codigo1);
-printf("populaçao: %d\n", populacao1);
-printf("area:%.2f em km²\n", area1);
-printf("PIB: R$ %.2f bilhoes\n", pib1);
-printf("pontos turisticos:%d\n", pontosturisticos1);
+printf("nome: %s\n",nome1); // exibir na tela o nome da cidade 1 digitada pelo usuario
+printf("estado: %s\n", estado1); // exibir na tela o estado da cidade 1 digitada pelo usuario
+printf("codigo: %d\n", codigo1); // exibir na tela o codigo da cidade 1 digitada pelo usuario
+printf("populaçao: %d\n", populacao1); // exibir na tela a populaçao da cidade 1 digitada pelo usuario
+printf("area:%.2f em km²\n", area1); // exibir na tela a aréa da cidade 1 digitada pelo usuario
+printf("PIB: R$ %.2f bilhoes\n", pib1); // exibir na tela o PIB da cidade 1 digitada pelo usuario
+printf("pontos turisticos:%d\n", pontosturisticos1); // exibir na tela os pontos turisticos da cidade 1 digitados pelo usuario
 
 // Carta 2 -- cidade 2
 printf("\n--- carta cidade 2---\n");
-printf("nome: %s\n",nome2); // mostra o nome
+printf("nome: %s\n",nome2); 
 printf("estado: %s\n", estado2);
 printf("codigo: %d\n", codigo2);
 printf("populaçao: %d\n", populacao2);
@@ -90,5 +90,5 @@ printf("pontos turisticos:%d\n", pontosturisticos2);
  
 
 
-    return 0;
+    return 0; // Indica que o programa foi executado com sucesso, sem erros
 }
